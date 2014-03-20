2a8354fd1d36afbdf746819376b34bfeecae0f21
Samuel Markoe <somarkoe@gmail.com>
2014-03-19 22:22:23 -0600

    Changed accusation test to accomodate for each individual part of the solution to be wrong, rather than the entire thing. Test still passing.

--- src/gameTests/GameActionTests.java
+++ src/gameTests/GameActionTests.java
@@ -23,9 +23,14 @@ static ClueGame game;
 		// Sets the solution to a discrete set of values
 		game.setSolution("Frodo", "Dagger", "The Shire");
 		
-		// Tests to make sure that the function won't return true when the solution does
-		// not match the correct values
-		assertFalse(game.checkAccusation(new Solution("Gollum", "The Ring", "Ash Mountains")));
+		// Tests to make sure that the function won't return true person isn't correct
+		assertFalse(game.checkAccusation(new Solution("Gollum", "Dagger", "The Shire")));
+		
+		// Tests to make sure that the function won't return true weapon isn't correct
+		assertFalse(game.checkAccusation(new Solution("Frodo", "Wizard Staff", "The Shire")));
+				
+		// Tests to make sure that the function won't return true room isn't correct
+		assertFalse(game.checkAccusation(new Solution("Frodo", "Dagger", "Rivendell")));
 		
 		// Verifies that the function will return true when the correct values are put in
 		assertTrue(game.checkAccusation(new Solution("Frodo", "Dagger", "The Shire")));
