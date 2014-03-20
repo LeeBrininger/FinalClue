44e2a518f96a33bc6fb865fe3d1b360b8c7ecbce
Samuel Markoe <somarkoe@gmail.com>
2014-03-19 22:20:09 -0600

    Added comments to accusation test

--- src/gameTests/GameActionTests.java
+++ src/gameTests/GameActionTests.java
@@ -19,9 +19,15 @@ static ClueGame game;
 	
 	@Test
 	public void testAccusation() {
-		game.deal();
+
+		// Sets the solution to a discrete set of values
 		game.setSolution("Frodo", "Dagger", "The Shire");
+		
+		// Tests to make sure that the function won't return true when the solution does
+		// not match the correct values
 		assertFalse(game.checkAccusation(new Solution("Gollum", "The Ring", "Ash Mountains")));
+		
+		// Verifies that the function will return true when the correct values are put in
 		assertTrue(game.checkAccusation(new Solution("Frodo", "Dagger", "The Shire")));
 	}
 
