c2eac247d6d4bb537cb8782dcd5b16cefef2046e
Carter Mann <cmann@mines.edu>
2014-03-21 19:19:04 -0600

    Passing target room and target consider last room tests

--- src/clue/ComputerPlayer.java
+++ src/clue/ComputerPlayer.java
@@ -6,17 +6,31 @@ import java.util.Set;
 public class ComputerPlayer extends Player {
 
 	private ArrayList<Card> unseenCards;
+	private char lastRoomVisited;
+	private BoardCell target;
 	
 	public ComputerPlayer(String name, String color, int startLocation) {
 		super(name, color, startLocation);
 		unseenCards = new ArrayList<Card>();
 	}
-
-	private char lastRoomVisited;
 	
 	
 	public BoardCell pickLocation(Set<BoardCell> targets) {
-		return null;
+		// In case of no room targets
+		target = targets[0];
+		
+		for (targets : i) {
+			i.next();
+			if (i.isRoom()) {
+				if (lastRoomVisited.equals(i.getCellCode)) {
+					// Do nothing if it's the last visited room
+					continue;
+				} else {
+					target = i;
+				}
+			}
+		}
+		return target;
 	}
 	
 	public Solution createSuggestion() {
